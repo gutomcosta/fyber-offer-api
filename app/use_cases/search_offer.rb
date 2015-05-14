c702ebfe47ac72a2d00a9289fc881866963dcc61
Luiz Costa <gutomcosta@gmail.com>
2015-05-14 01:18:42 -0300

    starts the use case logic implementation

--- app/use_cases/search_offer.rb
+++ app/use_cases/search_offer.rb
@@ -1,11 +1,18 @@
 class SearchOffer
 
-  def initialize
+  def initialize(haskey_calculator, offer_url, http_request)
+    @calculator     = haskey_calculator
+    @offer_url      = offer_url
+    @http_request   = http_request
   end
 
   def execute(data)
     raise ArgumentError, "missing search data" unless data_exists?(data) && is_valid?(data)
-    
+    hashkey  = @calculator.calculate(data)
+    url      = @offer_url.build(data, hashkey)
+    response = @http_request.request(url)
+    Offer.build(response.offers)
+
   end
 
   private 
--- spec/unit/use_cases/search_offer_spec.rb
+++ spec/unit/use_cases/search_offer_spec.rb
@@ -2,8 +2,22 @@ require 'rails_helper'
 
 describe SearchOffer do
   let(:valid_data) { {uid: "player1", pub0: "campaign2", page: "1"} }
+  let(:hashkey) { double }
+  let(:hashkey_calculator) { double }
+  let(:url) { double }
+  let(:offer_url) { double }
+  let(:response_data) { double }
+  let(:http_request) { double }
 
-  subject { SearchOffer.new }
+
+  subject { SearchOffer.new(hashkey_calculator, offer_url, http_request) }
+
+  before do 
+    allow(hashkey_calculator).to receive(:calculate).and_return(hashkey)
+    allow(offer_url).to receive(:build).and_return(url)
+    allow(http_request).to receive(:request).and_return(response_data)
+    allow(response_data).to receive(:offers).and_return([double, double])
+  end
 
   describe "#execute" do 
 
@@ -21,18 +35,32 @@ describe SearchOffer do
       end
 
       it "railse an ArgumentError if the data is invalid" do 
-        valid_data.delete(:uid)
-        invalid_data = valid_data
+        invalid_data = valid_data.clone
+        invalid_data.delete(:uid)
         expect {
           subject.execute(invalid_data)
         }.to raise_error ArgumentError
       end
     end
 
-    it "gets the hashkey calculated"
-    it "builds the URL to search offers"
-    it "makes an http request" 
-    it "builds a set of offers"
+    after do 
+      subject.execute(valid_data)
+    end
+
+    it "gets the hashkey calculated" do 
+      expect(hashkey_calculator).to receive(:calculate).with(valid_data)
+    end
+
+    it "builds the URL to search offers" do 
+      expect(offer_url).to receive(:build).with(valid_data, hashkey)
+    end
+
+    it "makes an http request" do 
+      expect(http_request).to receive(:request).with(url)
+    end
+    it "builds a set of offers" do 
+      expect(Offer).to receive(:build).with(response_data)
+    end
 
 
   end
