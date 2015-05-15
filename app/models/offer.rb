71cfc26723879c799b41e3fe8afa9babcb2306bf
Luiz Costa <gutomcosta@gmail.com>
2015-05-15 01:03:41 -0300

    implements the offer model

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,5 +1,18 @@
 class Offer
+  attr_reader :title, :thumbnail, :payout
+
   def self.build(data)
-    
+    offers    = data["offers"]
+    thumbnail = ""
+    offers.map do |offer|
+      thumbnail = offer["thumbnail"]["lowres"] if offer.has_key?("thumbnail")
+      Offer.new(offer["title"],thumbnail, offer["payout"])
+    end
+  end
+
+  def initialize(title, thumbnail, payout)
+    @title      = title
+    @thumbnail  = thumbnail
+    @payout     = payout
   end
 end
---
+++ spec/unit/models/offer_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+describe Offer do
+
+  let(:data) do 
+    {
+      "code"=>" OK",
+      "message"=>"OK",
+      "count"=>1,
+      "pages"=>1,
+      "information"=>
+      {"app_name"=>"SP Test App",
+        "appid"=>157,
+        "virtual_currency"=>"Coins",
+        "country"=>" US",
+        "language"=>" EN",
+        "support_url"=>"http://iframe.sponsorpay.com/mobile/DE/157/my_offers"},
+        "offers"=>
+          [{"title"=>"Tap  Fish",
+            "offer_id"=>13554,
+            "teaser"=>"Download and START",
+            "required_actions"=>"Download and START",
+            "link"=>"http://iframe.sponsorpay.com/mbrowser?appid=157&lpid=11387&uid=player1",
+            "offer_types"=>[{"offer_type_id"=>101, "readable"=>"Download"}, {"offer_type_id"=>112, "readable"=>"Free"
+          }],
+          "thumbnail"=>{
+            "lowres"=>"http://cdn.sponsorpay.com/assets/1808/icon175x175-2_square_60.png", "hires"=>"http://cdn.sponsorpay.com/assets/1808/icon175x175-2_square_175.png"},
+            "payout"=>90,
+            "time_to_payout"=>{"amount"=>1800, "readable"=>"30 minutes"}
+          }]
+      }
+  end
+
+  describe ".build" do 
+    
+    it "buids an Offer object from an Hash" do 
+      offer = Offer.build(data).first
+      expect(offer.title).to eq("Tap  Fish")
+      expect(offer.thumbnail).to eq("http://cdn.sponsorpay.com/assets/1808/icon175x175-2_square_60.png")
+      expect(offer.payout).to eq(90)
+    end
+
+  end
+
+end
